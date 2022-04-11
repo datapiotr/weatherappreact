0812b89091bf604298b48906e73a62bfcb5674bb
datapiotr <p.data@interia.pl>
2022-04-11 21:13:17 +0200

    getDate, getTemp functions added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const api = {
   key: 'fd9d6aab2c38c5938add09f03d6455aa',
   base: "https://api.openweathermap.org/data/2.5/"
@@ -5,20 +7,52 @@ const api = {
 
 function App() {
 
+  const [city, setCity] = useState('')
+  const [temp, setTemp] = useState({})
+
+
+  const getDate = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  const getTemp = (e) => {
+    if (e.key === "Enter" || e.type === "click") {
+      fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setTemp(result)
+          setCity('')
+          console.log(result)
+        })
+    }
+  }
 
   return (
     <div className="App">
       <main>
-        <section class="search-section">
+        <section className="search-section">
           <input
             type="text"
-            class="search-input"
+            className="search-input"
             placeholder="Type the city"
+            onChange={e => setCity(e.target.value)}
+            value={city}
+            onKeyPress={getTemp}
           />
-          <button class="search-btn">Check</button>
+          <button className="search-btn">Check</button>
         </section>
-        <section class="information">
-          <div class="data">London | 11.04.2022 | 10°c</div>
+        <section className="information">
+          {(typeof temp.main != 'undefined') ? (
+            <div className="data">{temp.name} | {getDate(new Date())} | {Math.round(temp.main.temp)}°c</div>
+          ) : ('')}
         </section>
       </main>
     </div>
